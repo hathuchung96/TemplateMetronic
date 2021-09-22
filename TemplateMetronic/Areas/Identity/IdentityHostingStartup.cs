9a8d40103fe7b94bcfead3139d4471820a0f2cf8
thuchung.ha@outlook.com <thuchung.ha@outlook.com>
2021-09-23 01:14:20 +0700

    Add project files.

---
+++ TemplateMetronic/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TemplateMetronic.Data;
+
+[assembly: HostingStartup(typeof(TemplateMetronic.Areas.Identity.IdentityHostingStartup))]
+namespace TemplateMetronic.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
